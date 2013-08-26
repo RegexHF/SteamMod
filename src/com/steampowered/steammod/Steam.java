16c65216e8168c3fe5128cf4fb214d2ba6283e3a
Regex_ <Regex_@hackforums.net>
2013-08-25 21:51:25 -0400

    README
    README

---
+++ src/com/steampowered/steammod/Steam.java
@@ -0,0 +1,18 @@
+package com.steampowered.steammod;
+
+public class Steam 
+{
+
+	private static Steam steam = new Steam();
+	
+	public Steam()
+	{
+		
+	}
+	
+	public static Steam getSteam()
+	{
+		return steam;
+	}
+	
+}
